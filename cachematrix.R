0821b90f49979dcb5b59528fcc3c1a3bcbfdca32
kegsofduff <kegsofduff@gmail.com>
2015-04-25 15:08:36 -0500

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do 
+# Using these 2 functions will allow for faster retieval of a previously 
+# solved inverse matrix
+
 
 ## Write a short comment describing this function
+# sets the value of the matrix and inverse of the matrix
+# allows for  retrival of maxtrix and cached inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
 ## Write a short comment describing this function
+# Checks if the inverse of the maxtrix has already been solved / cached
+# if so inverse is returned
+# if not inverse is calculated, cached, and returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
